9210b701f8b39a523f04631d5d5f07d73d0ce727
hsh07260 <40084349+hsh07260@users.noreply.github.com>
2019-11-09 18:02:03 +0900

    Create level1_4.cpp

---
+++ Programers/level1_4.cpp
@@ -0,0 +1,18 @@
+// Programers 가운데 글자 가져오기 문항
+// 간단한 문자 추출
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string s) {
+	string answer = "";
+	int s_len = s.length();
+	if (s_len % 2 == 0) {
+		answer.assign(s.begin() + s_len / 2 - 1, s.begin() + s_len / 2 + 1);
+	}
+	else {
+		answer = s[s_len / 2];
+	}
+	return answer;
+}
