723e8394d7bea8477ca75139d4f85059048558d2
hsh07260 <40084349+hsh07260@users.noreply.github.com>
2019-11-10 23:20:44 +0900

    Create level1_9.cpp

---
+++ Programers/level1_9.cpp
@@ -0,0 +1,18 @@
+// Programers 수박수박수? 문항
+// 기초 string 문제
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(int n) {
+	string answer = "";
+
+	for (int i = 0; i < n; i++) {
+		if (i % 2 == 0) answer += "수";
+		else answer += "박";
+	}
+
+	return answer;
+}
