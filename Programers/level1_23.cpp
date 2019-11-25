e75cc1ec62772c7917a4b93c1c75060593b514d7
hsh07260 <40084349+hsh07260@users.noreply.github.com>
2019-11-25 20:11:27 +0900

    정수 제곱근 판별 문항

---
+++ Programers/level1_23.cpp
@@ -0,0 +1,18 @@
+// Programers 정수 제곱근 판별 문항
+// cmath 사용
+#include <string>
+#include <vector>
+#include <cmath>
+using namespace std;
+
+long long solution(long long n) {
+	long long answer = 0;
+	long long n_int_sqrt = (long long)sqrt(n);
+	if (n == (long long)(n_int_sqrt * n_int_sqrt)) {
+		answer = (long long)((n_int_sqrt + 1)*(n_int_sqrt + 1));
+	}
+	else {
+		answer = -1;
+	}
+	return answer;
+}
