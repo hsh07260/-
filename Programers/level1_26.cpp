d0d9b0e758d981771aa8e2e734e81f808c4365be
hsh07260 <40084349+hsh07260@users.noreply.github.com>
2019-11-25 20:46:20 +0900

    하샤드 수 문항

---
+++ Programers/level1_26.cpp
@@ -0,0 +1,21 @@
+// Programers 하샤드 수 문항
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+bool solution(int x) {
+	bool answer = true;
+	int buf = x, pos_sum=0 ;
+	vector<int> arr;
+	while (buf != 0) {
+		pos_sum = pos_sum + (buf%10);
+		buf = buf / 10;
+	}
+	if (x%pos_sum != 0) {
+		return false;
+	}
+	
+	return answer;
+}
