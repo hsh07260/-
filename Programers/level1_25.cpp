2e35da6e0111530e5fb48d3d8b8bbd4aeaee4f00
hsh07260 <40084349+hsh07260@users.noreply.github.com>
2019-11-25 20:34:17 +0900

    콜라츠 추측 문항 / 변수형에 따른 오버플로우를 고려

---
+++ Programers/level1_25.cpp
@@ -0,0 +1,27 @@
+// Programers 콜라츠 추측 문항
+// num을 연산하는 과정에서 int형 범위를 벗어나기 때문에
+// 최소한 500번의 연산을해도될 정도의 정수형 변수를 선언하여 연산과정을 거쳐야 한다.
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int num) {
+	unsigned long long buf = (unsigned long long)num;
+	int answer = 0;
+	int cnt = 0;
+	
+	while (buf != 1) {
+		if (buf % 2 == 0) {
+			buf = buf / 2;
+		}
+		else {
+			buf = buf * 3 + 1;
+		}
+		cnt++;
+		if ((buf != 1) && (cnt > 500)) return -1;
+	}
+	answer = cnt;
+
+	return answer;
+}
