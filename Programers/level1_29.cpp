afef07dc86a2a3fd95a4629fe8e4732842485617
hsh07260 <40084349+hsh07260@users.noreply.github.com>
2019-11-27 20:26:17 +0900

    제일 작은 수 제거하기 문항

---
+++ Programers/level1_29.cpp
@@ -0,0 +1,15 @@
+// programers 제일 작은 수 제거하기 문항
+
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> solution(vector<int> arr) {
+	arr.erase(min_element(arr.begin(), arr.end()));
+	if (arr.empty()) {
+		return { -1 };
+	}
+	return arr;
+}
