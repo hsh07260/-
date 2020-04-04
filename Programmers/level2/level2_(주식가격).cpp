f9d31d4036c314af8cca4ddc5663203c91077c09
hsh07260 <40084349+hsh07260@users.noreply.github.com>
2020-04-04 20:11:46 +0900

    Create level2_(주식가격).cpp

---
+++ Programmers/level2/level2_(주식가격).cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> prices) {
+	vector<int> answer;
+	for (int i = 0; i < prices.size(); i++) {
+		int a = 0;
+		for (int j = i+1; j < prices.size(); j++) {
+            a++;
+			if (prices[i] > prices[j]) {
+				break;
+			}
+			
+		}
+		answer.push_back(a);
+	}
+	return answer;
+}
